139e13c529c959e656d15a6280012b73c606af02
kSabchev <kaloyansabchev@gmail.com>
2021-02-28 11:38:56 +0200

    fixed search page tags

--- chain-events/src/SearchPage.js
+++ chain-events/src/SearchPage.js
@@ -17,10 +17,11 @@ function SearchPage() {
       <div className="searchPage__info">
         <p>62 stays · 26 august to 30 august · 2 particpants</p>
         <h1>Stays nearby</h1>
-        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Covid safe environment</Button>
         <Button variant="outlined">Type of place</Button>
-        <Button variant="outlined">Price</Button>
-        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">Outdoors</Button>
+        <Button variant="outlined">Open event</Button>
+        <Button variant="outlined">Free parking</Button>
         <Button variant="outlined">More filters</Button>
       </div>
       <SearchResult
